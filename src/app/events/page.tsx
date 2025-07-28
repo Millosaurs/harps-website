4a8ee7d1fb18d94924371df04e684e12259ebaa9
Millosaurs <millosaurs@gmail.com>
2025-07-28 12:08:26 +0000

    update

--- src/app/events/page.tsx
+++ src/app/events/page.tsx
@@ -316,9 +316,25 @@ export default function MCCWebsite() {
         </div> */}
         <div className="container mx-auto px-24 ">
           <div className="text-left mb-12">
-            <h2 className="text-4xl font-extrabold text-white mb-2">
-              Creator Splash
+            <h2 className="relative text-4xl font-bold mb-2 tracking-wide">
+              <span
+                aria-hidden="true"
+                style={{
+                  position: "absolute",
+                  top: 0,
+                  left: 0,
+                  color: "transparent",
+                  WebkitTextStroke: "4px black",
+                  zIndex: 0,
+                }}
+              >
+                Creator Splash
+              </span>
+              <span style={{ position: "relative", color: "white", zIndex: 1 }}>
+                Creator Splash
+              </span>
             </h2>
+
             <h1
               className="text-9xl font-bold text-slate-900"
               style={{ fontFamily: "Impact, sans-serif" }}
